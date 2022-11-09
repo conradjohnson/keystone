8927c25ec01e1a093c27c69983fdb495f666cf1e
conradjohnson <skaterjames@gmail.com>
2022-11-09 12:32:06 -0600

    resolvers and typedefs mostly done for initial stuff

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+//prod: process.env.MONGODB_URI 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/keystone', {
+mongoose.connect(process.env.TESTMONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });

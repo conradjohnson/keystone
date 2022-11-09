d14309e1b2d6d355192a9748d8e6af4ce19837f9
conradjohnson <skaterjames@gmail.com>
2022-11-09 12:05:33 -0600

    resetup the directory structure from Activity 26 in State module.  Setup initial models.  Need to do resolvers and typedefs next.

---
+++ server/models/Property.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const propertySchema = new Schema({
+  address: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  address2: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  city: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  state: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  country: {
+    type: String,
+    default: 'USA',
+    trim: true
+  },
+  zip: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  value: {
+    type: Float
+  },
+  images: [{
+    type: String
+  }],
+  forSale: {
+    type: Boolean,
+    default: false
+  },
+  salePrice: {
+    type: Float,
+    default: -1
+  }
+});
+
+const Property = mongoose.model('Property', propertySchema);
+
+module.exports = Property;

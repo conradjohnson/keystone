e7bd664f78b8d6c0819c798691e5f871dcd5697b
conradjohnson <skaterjames@gmail.com>
2022-11-10 11:50:05 -0600

    added user session check functionality to profile and add-property

--- client/src/pages/AddProperty.js
+++ client/src/pages/AddProperty.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
 import { QUERY_USER } from '../utils/queries';
 
 
-function AddProperty(){
+function AddProperty(props){
+
+    const { data } = useQuery(QUERY_USER);
+    let user;
+
+    if (data) {
+        user = data.user;
+    }
+    const [formState, setFormState] = useState({ email: '', password: '' });
+
     return (
+        Auth.loggedIn() ? (
         <>
           <div className="container my-1">
            Add Property Form here
            <form><input type="text" name="address" placeholder="123 Main" />
            <button type="submit" value="submit!">nnnn</button></form>
           </div>
-        </>)
+        </> 
+        ) : null )
             
 }
 
